5f07ce04c532937f272c0ad95571df6ca4a9c6db
Brendan Milton <brendanmilton@MacBooks-MacBook-Air.local>
2017-12-04 14:02:37 +0000

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Brendan Milton on 04/12/2017.
+//  Copyright © 2017 Brendan Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
